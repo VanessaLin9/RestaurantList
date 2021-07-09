5ef0662c500c152dd5698b66bada099b5c8b61b0
VanessaLin9 <vanessa7591@gmail.com>
2021-07-09 11:04:56 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/RestaurantList', { useNewUrlParser: true, useUnifiedTopology: true })
 const restaurantList = require('./restaurant.json')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+
+//取得資料庫連線狀態
+const db = mongoose.connection
+//連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+//連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.render('index', { restaurant: restaurantList.results })
