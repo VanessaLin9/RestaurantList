2b57a4988bdcbe84975c9c06035e20c8aaf8255c
VanessaLin9 <vanessa7591@gmail.com>
2021-07-10 04:40:08 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+// 引用 body-parser
+const bodyParser = require('body-parser')
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 //取得資料庫連線狀態
 const db = mongoose.connection
 //連線異常
@@ -29,8 +34,27 @@ db.once('open', () => {
 app.get('/', (req, res) => {
   Res.find() //取出Res model裡面的資料
     .lean() //把mongoose拿出來的資料弄成乾淨的JS資料陣列
-    .then( resList => res.render('index', {resList}))
-    .catch( error => console.error(error))
+    .then(resList => res.render('index', { resList }))
+    .catch(error => console.error(error))
+})
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return Res.create({ name, name_en, category, image, location, phone, google_map, rating, description})
+  .then(() => res.redirect('/'))
+  .catch(error => console.log(error))
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
