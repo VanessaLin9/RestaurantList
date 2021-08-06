d34d2414b51e66493830260568c59a24ffc1f215
VanessaLin9 <vanessa7591@gmail.com>
2021-08-07 07:06:11 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,15 @@ const express = require('express')
 const router = express.Router()
 // 準備引入路由模組
 
-// 引入兩個頁面模組
+// 引入各頁面模組
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
-
+const users = require('./modules/users')
 
 // 將網址結構符合 / 字串的 request 導向各模組 
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 
 // 匯出路由器
