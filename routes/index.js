cc91aaf3a7fc3fac85f49927aa20595623b1028b
VanessaLin9 <vanessa7591@gmail.com>
2021-08-08 06:26:21 +0800

    feat: add user authentication in restaurants and home routers

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,12 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
 // 將網址結構符合 / 字串的 request 導向各模組 
-router.use('/', home)
-router.use('/restaurants', restaurants)
+router.use('/restaurants', authenticator, restaurants)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 
 // 匯出路由器
