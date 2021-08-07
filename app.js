1fdfe2b2d31ba3153bffdc1794a694e1543ef531
VanessaLin9 <vanessa7591@gmail.com>
2021-08-08 04:12:44 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 const express = require('express')
-
-const port = 3000
-const exphbs = require('express-handlebars') //使用handlebar
-const bodyParser = require('body-parser') // 引用 body-parser
-const methOverride = require('method-override') //載入method-override
-
-// 引用路由器
-const routes = require('./routes')
-const app = express()
+const session = require('express-session')
+const exphbs = require('express-handlebars') 
+const bodyParser = require('body-parser') 
+const methOverride = require('method-override') 
 const isEqual = require('./tools/handlebarsHelper')
 
+const routes = require('./routes')
 require('./config/mongoose')
 
+const app = express()
+const port = 3000
+
 app.use(express.static('public'))
-
-
-// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
-app.use(bodyParser.urlencoded({ extended: true }))
-
 
 app.engine('handlebars', exphbs({ 
   defaultLayout: 'main',
   helpers: { isEqual }
   }))
 app.set('view engine', 'handlebars')
-//設定每一筆請求都會通過method override進行前處理
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methOverride('_method'))
 
 
-// 將 request 導入路由器
+
 app.use(routes)
 
 app.listen(port, () => {
