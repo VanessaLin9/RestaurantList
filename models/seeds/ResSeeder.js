31699d0cda4c6029ac4e891a8fbbde5f2b782ffe
VanessaLin9 <vanessa7591@gmail.com>
2021-08-08 22:59:39 +0800

    modify ResSeeder

--- models/seeds/ResSeeder.js
+++ models/seeds/ResSeeder.js
@@ -1,24 +1,51 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Restaurant = require('../restaurant') 
-const resList = require('./restaurant.json')
+const User = require('../user')
 const db = require('../../config/mongoose')
 
+const resList = require('./restaurant.json').results
+const restaurant = require('../restaurant')
+
+const SEED_USERs = [{
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678',
+  ownRestaurants:[1, 2, 3]},
+{
+  name: 'user2',
+  email: 'user2@example.com',
+  password: '12345678',
+  ownRestaurants: [4, 5, 6]
+}]
+
+
+
 db.once('open', () => {
-  console.log('mongodb connected!')
-  for (let i = 0; i < 8; i++) {
-    let reslist = resList.results[i]
-    Restaurant.create({
-      name: reslist.name,
-      name_en: reslist.name_en,
-      category: reslist.category,
-      image: reslist.image,
-      location: reslist.location,
-      phone: reslist.phone,
-      google_map: reslist.google_map,
-      rating: reslist.rating,
-      description: reslist.description
+ 
+  Promise.all(SEED_USERs.map(SEED_USER =>
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+
+    .then(user => {    
+        const userRestaurant = resList.filter(restaurant => SEED_USER.ownRestaurants.includes(restaurant.id))
+        userRestaurant.forEach(restaurant => {restaurant.userId = user._id})
+      return Restaurant.create(userRestaurant)
+        })
+  ))
+    .then(() => {
+      console.log('done.')
+      process.exit()
     })
-  }
-  console.log('done')
+  .catch(error => console.log(error))
 })
 
 
